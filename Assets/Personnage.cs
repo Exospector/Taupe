1690020614239502255ac6914085e77273e39597
miyoku157 <miyoku157@gmail.com>
2015-11-05 12:55:07 +0100

    Déplacement

--- Assets/Personnage.cs
+++ Assets/Personnage.cs
@@ -31,6 +31,8 @@ public class Personnage : MonoBehaviour
 			if((Input.GetTouch (0).phase == TouchPhase.Stationary) || (Input.GetTouch (0).phase == TouchPhase.Moved && Input.GetTouch (0).deltaPosition.magnitude < 2))
 			{
 				Vector2 touchPosition = camera.ScreenToWorldPoint(Input.GetTouch (0).position);
+				touchPosition.x=touchPosition.x-transform.position.x;
+				touchPosition.y=touchPosition.y-transform.position.y;
 				// Move object across XY plane
 				transform.Translate (touchPosition.x * speed, touchPosition.y * speed, 0);
 			}
